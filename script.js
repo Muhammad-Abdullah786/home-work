284ed25d2b6ca542e0e1e532de81e5e707848eaf
Muhammad Abdullah <abdulx125@gmail.com>
2023-09-28 18:53:29 -0700

    did't do anything

--- script.js
+++ script.js
@@ -8,12 +8,5 @@ console.log("connexted");
 //     console.log(i);
 //     i++;
 // } while (i < 10);
-function generatePassword(len){
-    let password ="";
-    let charSet = "qwertyuioplkjhgfdsazxcvbnmAZXCVBNMLKJDGHFQWERTYUIO!@#$%^~&*";
-    for (let i = 0; i<charSet.length; i++) {
-        let set = charSet[Math.floor(Math.random()*charSet.length)];// pixk random characters from charset  
-        let ranIndex = Math.floor(Math.random() * set.length);
-        password += 
-    }
-}
+
+
