5eb6e8c257256d4e3dc63ef2bb45be52b84095bd
Muhammad Abdullah <abdulx125@gmail.com>
2023-09-28 18:23:45 -0700

    another easy peasy way

--- script.js
+++ script.js
@@ -8,26 +8,12 @@ console.log("connexted");
 //     console.log(i);
 //     i++;
 // } while (i < 10);
-
-// make an attay of integers and  make an array of odd numbers and make an array of even numbers  and  push them into the array of  integers use while loop
-
-
-let intArr = [1,2,4,5,6,7,8,9,10,11,12,13,14];
-let oddArr = [];
-let evenArr = [];
-
-while (true) {
-    if (intArr.length === 0) {
-        break;
-    }
-    if (intArr[0] % 2 === 0) {
-        evenArr.push(intArr.shift());
-    } else {
-        oddArr.push(intArr.shift());
+function generatePassword(len){
+    let password ="";
+    let charSet = "qwertyuioplkjhgfdsazxcvbnmAZXCVBNMLKJDGHFQWERTYUIO!@#$%^~&*";
+    for (let i = 0; i<charSet.length; i++) {
+        let set = charSet[Math.floor(Math.random()*charSet.length)];// pixk random characters from charset  
+        let ranIndex = Math.floor(Math.random() * set.length);
+        password += 
     }
 }
-
-console.log(`this is integer array\n: ${intArr}`);
-console.log(`this is even array\n: ${evenArr}`);
-console.log(`this is odd array\n: ${oddArr}`);
-
