98f7346ee4b9ab15d4126e201b57eb65ee33a492
Muhamad Abdullah <abdulx125@gmail.com>
2023-10-20 19:50:54 -0700

    learned recurtion

--- script.js
+++ script.js
@@ -11,33 +11,47 @@ console.log("connexted");
 
 
 
-// adding event listeners to the list
-let homeNode = document.getElementById('home');
-let aboutNode = document.getElementById('about');
-let contactNode = document.getElementById('contact');
-let desc = document.getElementsByClassName('desc');
+// // adding event listeners to the list
+// let homeNode = document.getElementById('home');
+// let aboutNode = document.getElementById('about');
+// let contactNode = document.getElementById('contact');
+// let desc = document.getElementsByClassName('desc');
 
-homeNode.addEventListener('click', () => {
-    console.log('home clicked');
-    let h = document.createElement('h1');
-    h.innerHTML = 'this is home';
-    // now we append in to the desc element
-    desc[0].appendChild(h);
+// homeNode.addEventListener('click', () => {
+//     console.log('home clicked');
+//     let h = document.createElement('h1');
+//     h.innerHTML = 'this is home';
+//     // now we append in to the desc element
+//     desc[0].appendChild(h);
 
 
     
 
-});
-aboutNode.addEventListener('click', () => {
-    let a = document.createElement('p');
-    a.textContent = 'this is about loremdsjfashkfjsdkfjsdkfjadlkjflkj';
-    aboutNode.appendChild(a);
-    console.log( "about clicked");
-})
+// });
+// aboutNode.addEventListener('click', () => {
+//     let a = document.createElement('p');
+//     a.textContent = 'this is about loremdsjfashkfjsdkfjsdkfjadlkjflkj';
+//     aboutNode.appendChild(a);
+//     console.log( "about clicked");
+// })
 
-contactNode.addEventListener('click', () => {
-    let c = document.createElement('p');
-    c.textContent = 'this is contact';
-    contactNode.appendChild(c);
-    console.log( "contact clicked");
-})
+// contactNode.addEventListener('click', () => {
+//     let c = document.createElement('p');
+//     c.textContent = 'this is contact';
+//     contactNode.appendChild(c);
+//     console.log( "contact clicked");
+// })
+
+// recursion
+
+var count = 0
+function recursion(){
+    count ++
+    if (count === 30) {
+        return
+    }
+
+    console.log(count * 2); // what you want to do? here i amd multiplyting 
+    recursion() // recursive call 
+}
+recursion() // calling the function normally
