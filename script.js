a3469501810c6f5726f45d23b782400ee5f00186
Muhamad Abdullah <abdulx125@gmail.com>
2023-10-28 00:21:40 -0700

    code war problem of unique character completed

--- script.js
+++ script.js
@@ -59,14 +59,35 @@ console.log("connexted");
 
 
 
-function hello() {
-  let a ='eawwass' 
-  console.log(a)
+// function hello() {
+//   let a ='eawwass' 
+//   console.log(a)
+// }
+// console.log(hello());
+
+function expandLoris() {
+   var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine primates which make up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety of distinctive coloration patterns that are species-dependent. The hands and feet of slow lorises have several adaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time. Slow lorises have a toxic bite, a rare trait among mammals.";
+  document.getElementById("slowLoris").innerHTML = expandedParagraph;
 }
-console.log(hello());
 
 
 
+// Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+// The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+
+function uniqueChars(str) {
+  var chars = str.split("");
+    var uniqueChars = true;
+    for (var i = 0; i < chars.length; i++) {
+        for (var j = i + 1; j < chars.length; j++) {
+            if (chars[i] === chars[j]) {
+                uniqueChars = false;
+            }
+        }
+    } 
+    return uniqueChars;
+  }
+  console.log(uniqueChars("abcdef"));
 
 
 
