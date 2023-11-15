3766a074847e9df121aa81a62757414b8cb82108
Muhamad Abdullah <abdulx125@gmail.com>
2023-11-14 20:04:57 -0800

    recursion

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ console.log("connexted");
 //     desc[0].appendChild(h);
 
 
-    
+
 
 // });
 // aboutNode.addEventListener('click', () => {
@@ -66,7 +66,7 @@ console.log("connexted");
 // console.log(hello());
 
 function expandLoris() {
-   var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine primates which make up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety of distinctive coloration patterns that are species-dependent. The hands and feet of slow lorises have several adaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time. Slow lorises have a toxic bite, a rare trait among mammals.";
+  var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine primates which make up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety of distinctive coloration patterns that are species-dependent. The hands and feet of slow lorises have several adaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time. Slow lorises have a toxic bite, a rare trait among mammals.";
   document.getElementById("slowLoris").innerHTML = expandedParagraph;
 }
 
@@ -77,229 +77,187 @@ function expandLoris() {
 
 function uniqueChars(str) {
   var chars = str.split("");
-    var uniqueChars = true;
-    for (var i = 0; i < chars.length; i++) {
-        for (var j = i + 1; j < chars.length; j++) {
-            if (chars[i] === chars[j]) {
-                uniqueChars = false;
-            }
-        }
-    } 
-    return uniqueChars;
-  }
-console.log("connexted");
-
-
-// do while loop
-
-// let i = 0;
-// do {
-//     console.log(i);
-//     i++;
-// } while (i < 10);
-
-
-
-// // adding event listeners to the list
-// let homeNode = document.getElementById('home');
-// let aboutNode = document.getElementById('about');
-// let contactNode = document.getElementById('contact');
-// let desc = document.getElementsByClassName('desc');
-
-// homeNode.addEventListener('click', () => {
-//     console.log('home clicked');
-//     let h = document.createElement('h1');
-//     h.innerHTML = 'this is home';
-//     // now we append in to the desc element
-//     desc[0].appendChild(h);
-
-
-    
-
-// });
-// aboutNode.addEventListener('click', () => {
-//     let a = document.createElement('p');
-//     a.textContent = 'this is about loremdsjfashkfjsdkfjsdkfjadlkjflkj';
-//     aboutNode.appendChild(a);
-//     console.log( "about clicked");
-// })
-
-// contactNode.addEventListener('click', () => {
-//     let c = document.createElement('p');
-//     c.textContent = 'this is contact';
-//     contactNode.appendChild(c);
-//     console.log( "contact clicked");
-// })
-
-// recursion
-
-// var count = 0
-// function recursion(){
-//     count ++
-//     if (count === 30) {
-//         return // if the c
-//     }
-
-//     console.log(count * 2); // what you want to do? here i amd multiplying 
-//     recursion() // recursive call 
-// }
-// recursion() // calling the function normally
-
-
-
-
-// function hello() {
-//   let a ='eawwass' 
-//   console.log(a)
-// }
-// console.log(hello());
-
-function expandLoris() {
-   var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine primates which make up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety of distinctive coloration patterns that are species-dependent. The hands and feet of slow lorises have several adaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time. Slow lorises have a toxic bite, a rare trait among mammals.";
-  document.getElementById("slowLoris").innerHTML = expandedParagraph;
-}
-
-
-
-// Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
-// The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
-
-function uniqueChars(str) {
-  var chars = str.split("");
-    var uniqueChars = true;
-    for (var i = 0; i < chars.length; i++) {
-        for (var j = i + 1; j < chars.length; j++) {
-            if (chars[i] === chars[j]) {
-                uniqueChars = false;
-            }
-        }
-    } 
-    return uniqueChars;
-  }
-console.log("connexted");
-
-
-// do while loop
-
-// let i = 0;
-// do {
-//     console.log(i);
-//     i++;
-// } while (i < 10);
-
-
-
-// // adding event listeners to the list
-// let homeNode = document.getElementById('home');
-// let aboutNode = document.getElementById('about');
-// let contactNode = document.getElementById('contact');
-// let desc = document.getElementsByClassName('desc');
-
-// homeNode.addEventListener('click', () => {
-//     console.log('home clicked');
-//     let h = document.createElement('h1');
-//     h.innerHTML = 'this is home';
-//     // now we append in to the desc element
-//     desc[0].appendChild(h);
-
-
-    
-
-// });
-// aboutNode.addEventListener('click', () => {
-//     let a = document.createElement('p');
-//     a.textContent = 'this is about loremdsjfashkfjsdkfjsdkfjadlkjflkj';
-//     aboutNode.appendChild(a);
-//     console.log( "about clicked");
-// })
-
-// contactNode.addEventListener('click', () => {
-//     let c = document.createElement('p');
-//     c.textContent = 'this is contact';
-//     contactNode.appendChild(c);
-//     console.log( "contact clicked");
-// })
-
-// recursion
-
-// var count = 0
-// function recursion(){
-//     count ++
-//     if (count === 30) {
-//         return // if the c
-//     }
-
-//     console.log(count * 2); // what you want to do? here i amd multiplying 
-//     recursion() // recursive call 
-// }
-// recursion() // calling the function normally
-
-
-
-
-// function hello() {
-//   let a ='eawwass' 
-//   console.log(a)
-// }
-// console.log(hello());
-
-function expandLoris() {
-   var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine primates which make up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety of distinctive coloration patterns that are species-dependent. The hands and feet of slow lorises have several adaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time. Slow lorises have a toxic bite, a rare trait among mammals.";
-  document.getElementById("slowLoris").innerHTML = expandedParagraph;
-}
-
-
-
-// Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
-// The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
-
-function uniqueChars(str) {
-  var chars = str.split("");
-    var uniqueChars = true;
-    for (var i = 0; i < chars.length; i++) {
-        for (var j = i + 1; j < chars.length; j++) {
-            if (chars[i] === chars[j]) {
-                uniqueChars = false;
-            }
-        }
-    } 
-    return uniqueChars;
-  }
-  
-  console.log(uniqueChars("abcdef"));
-
-
-
-//   Given two arrays of strings, return the number of times each string of the second array appears in the first array.
-
-// Example
-// array1 = ['abc', 'abc', 'xyz', 'cde', 'uvw']
-// array2 = ['abc', 'cde', 'uap']
-// How many times do the elements in array2 appear in array1?
-
-// 'abc' appears twice in the first array (2)
-// 'cde' appears only once (1)
-// 'uap' does not appear in the first array (0)
-// Therefore, solve(array1, array2) = [2, 1, 0]
-
-function solve(array1, array2) {
-  let result = [];
-  for (let i = 0; i < array2.length; i++) {
-    let count = 0;
-    for (let j = 0; j < array1.length; j++) {
-      if (array2[i] === array1[j]) {
-        count++;
+  var uniqueChars = true;
+  for (var i = 0; i < chars.length; i++) {
+    for (var j = i + 1; j < chars.length; j++) {
+      if (chars[i] === chars[j]) {
+        uniqueChars = false;
       }
     }
-    result.push(count);
   }
-  return result;
+  return uniqueChars;
+}
+console.log("connexted");
+
+
+// do while loop
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 10);
+
+
+
+// // adding event listeners to the list
+// let homeNode = document.getElementById('home');
+// let aboutNode = document.getElementById('about');
+// let contactNode = document.getElementById('contact');
+// let desc = document.getElementsByClassName('desc');
+
+// homeNode.addEventListener('click', () => {
+//     console.log('home clicked');
+//     let h = document.createElement('h1');
+//     h.innerHTML = 'this is home';
+//     // now we append in to the desc element
+//     desc[0].appendChild(h);
+
+
+
+
+// });
+// aboutNode.addEventListener('click', () => {
+//     let a = document.createElement('p');
+//     a.textContent = 'this is about loremdsjfashkfjsdkfjsdkfjadlkjflkj';
+//     aboutNode.appendChild(a);
+//     console.log( "about clicked");
+// })
+
+// contactNode.addEventListener('click', () => {
+//     let c = document.createElement('p');
+//     c.textContent = 'this is contact';
+//     contactNode.appendChild(c);
+//     console.log( "contact clicked");
+// })
+
+// recursion
+
+// var count = 0
+// function recursion(){
+//     count ++
+//     if (count === 30) {
+//         return // if the c
+//     }
+
+//     console.log(count * 2); // what you want to do? here i amd multiplying 
+//     recursion() // recursive call 
+// }
+// recursion() // calling the function normally
+
+
+
+
+// function hello() {
+//   let a ='eawwass' 
+//   console.log(a)
+// }
+// console.log(hello());
+
+function expandLoris() {
+  var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine primates which make up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety of distinctive coloration patterns that are species-dependent. The hands and feet of slow lorises have several adaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time. Slow lorises have a toxic bite, a rare trait among mammals.";
+  document.getElementById("slowLoris").innerHTML = expandedParagraph;
 }
 
-console.log(solve(['abc', 'abc', 'xyz', 'cde', 'uvw'], ['abc', 'cde', 'uap']));
+
+
+// Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+// The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+
+function uniqueChars(str) {
+  var chars = str.split("");
+  var uniqueChars = true;
+  for (var i = 0; i < chars.length; i++) {
+    for (var j = i + 1; j < chars.length; j++) {
+      if (chars[i] === chars[j]) {
+        uniqueChars = false;
+      }
+    }
+  }
+  return uniqueChars;
+}
+console.log("connexted");
+
+
+// do while loop
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 10);
+
+
+
+// // adding event listeners to the list
+// let homeNode = document.getElementById('home');
+// let aboutNode = document.getElementById('about');
+// let contactNode = document.getElementById('contact');
+// let desc = document.getElementsByClassName('desc');
+
+// homeNode.addEventListener('click', () => {
+//     console.log('home clicked');
+//     let h = document.createElement('h1');
+//     h.innerHTML = 'this is home';
+//     // now we append in to the desc element
+//     desc[0].appendChild(h);
 
 
 
 
+// });
+// aboutNode.addEventListener('click', () => {
+//     let a = document.createElement('p');
+//     a.textContent = 'this is about loremdsjfashkfjsdkfjsdkfjadlkjflkj';
+//     aboutNode.appendChild(a);
+//     console.log( "about clicked");
+// })
 
+// contactNode.addEventListener('click', () => {
+//     let c = document.createElement('p');
+//     c.textContent = 'this is contact';
+//     contactNode.appendChild(c);
+//     console.log( "contact clicked");
+// })
+
+// recursion
+
+var count = 0
+function recursion(){
+    count ++
+    if (count === 30) {
+        return // if the c
+    }
+
+    console.log(count * 2); // what you want to do? here i amd multiplying 
+    recursion() // recursive call 
+}
+recursion() // calling the function normally
+
+
+
+
+// function hello() {
+//   let a ='eawwass' 
+//   console.log(a)
+// }
+// console.log(hello());
+
+function expandLoris() {
+  var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine primates which make up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety of distinctive coloration patterns that are species-dependent. The hands and feet of slow lorises have several adaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time. Slow lorises have a toxic bite, a rare trait among mammals.";
+  document.getElementById("slowLoris").innerHTML = expandedParagraph;
+}
+function accumulate(str) {
+  let results = [];
+  string = str.split('');
+  for (let i = 0; i < str.length - 1; i++) {
+    results.push(str[i] + i );  
+    console.log(results);
+
+
+  }
+  accumulate();
+
+}
+console.log(accumulate('abcd'));
 
